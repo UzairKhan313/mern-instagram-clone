6e292f807056b33ad11a899c60fbbc0ceaf2f9ce
UzairKhan313 <uzairkhaan2003@gmail.com>
2024-10-29 18:14:35 +0500

    initial commit for starting client side

---
+++ client/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  darkMode: ["class"],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", // Adjust to your project's structure
+    "./node_modules/@shadcn/**/*.{js,ts,jsx,tsx}", // Add ShadCN's path here
+  ],
+  theme: {
+    extend: {
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
+      colors: {},
+    },
+  },
+  plugins: [require("tailwindcss-animate")],
+};
